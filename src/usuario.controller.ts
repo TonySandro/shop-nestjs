06a680e3f6510fea4dbb09b74848c57cf1fb287a
Tony Sandro <tonysduarte101@gmail.com>
2023-03-20 10:01:53 -0300

    feat: add user repository

--- src/usuario.controller.ts
+++ src/usuario.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { UsuarioRepository } from './usuario.repository';
+import { Controller, Post } from '@nestjs/common';
+import { Body } from '@nestjs/common/decorators';
 
 @Controller('/usuarios')
 export class UsuarioController {
-  async criaUsuario() {}
+  private usuarioRepository = new UsuarioRepository();
+
+  @Post()
+  async criaUsuario(@Body() dadosDoUsuario) {
+    this.usuarioRepository.salvar(dadosDoUsuario);
+    return dadosDoUsuario;
+  }
 }
